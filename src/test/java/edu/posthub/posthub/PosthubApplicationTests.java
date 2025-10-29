cd51cb45863eaa44db558aa914f2dc3deb83f874
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2025-10-29 18:27:01 +0000

    Initial commit

---
+++ src/test/java/edu/posthub/posthub/PosthubApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.posthub.posthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosthubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
