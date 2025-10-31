e5c0e1a18eb34545f5cce15cb3434a70f3794c03
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2025-10-31 19:39:49 +0000

    Add: User DTO

---
+++ src/main/java/edu/posthub/posthub/dtos/CreateUserDTO.java
@@ -0,0 +1,5 @@
+package edu.posthub.posthub.dtos;
+
+public record CreateUserDTO(String username, String email, String password) {
+
+}
---
+++ src/main/java/edu/posthub/posthub/dtos/UserDTO.java
@@ -0,0 +1,4 @@
+package edu.posthub.posthub.dtos;
+
+public record UserDTO(Long id, String username) {
+}
