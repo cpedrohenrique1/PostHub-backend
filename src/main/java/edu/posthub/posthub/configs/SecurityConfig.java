2ead10d1637e6e08d4e592302e53126bac32420b
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2025-11-06 00:07:32 -0300

    feat: update CORS configuration to allow additional origins

--- src/main/java/edu/posthub/posthub/configs/SecurityConfig.java
+++ src/main/java/edu/posthub/posthub/configs/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173", "http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173", "http://localhost:4200", "http://localhost:8081", "https://localhost:8081"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
